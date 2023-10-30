0cf5c1c44ad4c2c3a372133ec330036cf1270da2
grace2401 <akshitha.kokkiligadda@cogoport.com>
2023-10-30 17:25:43 +0530

    frontend

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,20 @@
 import express, { request, response } from "express";
 import { port, MONGODBURL } from "./config.js";
 import mongoose from "mongoose";
-import {Book} from "./models/bookModel.js";
 import booksRoute from './routes/bookRoute.js';
+import cors from 'cors';
+
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
+// app.use(cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET','POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+// }));
 
 app.get('/',(request, response)=>{
     console.log(request);
